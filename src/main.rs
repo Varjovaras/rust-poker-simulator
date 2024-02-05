f46fa7b0d151911b5a0586b3f016c22756ddbd6a
Kristjan Rajaniemi <kristjan.rajaniemi@live.fi>
2024-02-05 11:10:22 +0200

    formatter for leptos view! macro

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,24 @@ pub mod deck;
 pub mod poker;
 
 fn main() {
-    mount_to_body(|| view! { <p>"Hello, world!"</p> });
-
-    let mut poker = Poker::new_texas_hold_em(4, 0); // Call
-    for i in 0..1_000_000 {
-        poker.deck.shuffle();
-        let hand_value = Hand::new(&poker.deck.cards[0..2], &poker.deck.cards[2..7]);
-
-        if i % 10000 == 0 {
-            println!("{:?}", poker.deck.cards[0..7].to_vec());
-            println!("{hand_value:?}");
+    console_error_panic_hook::set_once();
+    mount_to_body(|| {
+        view! {
+            <p>"Hello, world!"</p>
+            <div>ali ali :D</div>
         }
-    }
+    });
 
-    println!("{:?}", poker.deck.cards[0..7].to_vec());
+    // let mut poker = Poker::new_texas_hold_em(4, 0); // Call
+    // for i in 0..1_000_000 {
+    //     poker.deck.shuffle();
+    //     let hand_value = Hand::new(&poker.deck.cards[0..2], &poker.deck.cards[2..7]);
+
+    //     if i % 10000 == 0 {
+    //         println!("{:?}", poker.deck.cards[0..7].to_vec());
+    //         println!("{hand_value:?}");
+    //     }
+    // }
+
+    // println!("{:?}", poker.deck.cards[0..7].to_vec());
 }
