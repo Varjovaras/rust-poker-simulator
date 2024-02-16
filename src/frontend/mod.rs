528d7e9d72b0dd892f768ddb369235339446d345
Kristjan Rajaniemi <kristjan.rajaniemi@live.fi>
2024-02-16 12:12:20 +0200

    derive players from poker state

--- src/frontend/mod.rs
+++ src/frontend/mod.rs
@@ -10,59 +10,52 @@ fn App() -> impl IntoView {
     let poker = Poker::new_texas_hold_em(4, 0);
     let (poker_state, set_poker_state) = create_signal(poker);
     poker_signal_shuffle(set_poker_state);
-    let (players, set_players) = create_signal(Vec::new());
+    // let (players, set_players) = create_signal(Vec::new());
+    let players = move || poker_state().board.players;
 
     view! {
         <div>
             <h1>{"Poker"}</h1>
-            <PokerTable players=(players, set_players)/>
+            <PokerTable players=Signal::derive(players) set_poker_state=set_poker_state/>
 
-            <button on:click=move |_| {
-                deal_new_hand(set_poker_state, set_players);
-                console_log("new hand");
-            }>
+        // <button on:click=move |_| {
+        // deal_new_hand(set_poker_state);
+        // console_log("new hand");
+        // }>
 
-                {"new hand"}
-            </button>
+        // {"new hand"}
+        // </button>
         </div>
     }
 }
 
-fn deal_new_hand(set_poker_state: WriteSignal<Poker>, set_players: WriteSignal<Vec<Player>>) {
+fn deal_new_hand(set_poker_state: WriteSignal<Poker>) {
     set_poker_state.update(|poker| {
         poker.new_deck();
         poker.shuffle();
         poker.deal_all_players();
         console_log(poker.board.players[0].get_hand_as_str().as_str());
-        set_players.set(poker.board.players.clone());
     });
 }
 
 #[component]
-fn PokerTable(players: (ReadSignal<Vec<Player>>, WriteSignal<Vec<Player>>)) -> impl IntoView {
-    let (players_state, set_players_state) = players;
-    let players = move || {
-        players_state
-            .get()
-            .iter()
-            .map(|item| (item.id, item.clone()))
-            .collect::<Vec<_>>()
-    };
+fn PokerTable(
+    #[prop(into)] players: Signal<Vec<Player>>,
+    set_poker_state: WriteSignal<Poker>,
+) -> impl IntoView {
+    // let players = players.get();
+
     view! {
         <ul class="todo-list">
             <For
                 each=players
-                key=|todo_key| todo_key.0
+                key=|todo_key| todo_key.id
                 children=move |player| {
                     view! {
                         <li class="new-todo">
-                            <p>{player.1.name.clone()}</p>
-                            {player.1.get_hand_as_str()}
-                            <button on:click=move |_| {
-                                let mut new_players = players_state.get();
-                                new_players.retain(|x| x.id != player.0);
-                                set_players_state.set(new_players);
-                            }>{"fold"}</button>
+                            <p>{player.name.clone()}</p>
+                            {player.get_hand_as_str()}
+                            <button on:click=move |_| {}>{"fold"}</button>
                         </li>
                     }
                 }
